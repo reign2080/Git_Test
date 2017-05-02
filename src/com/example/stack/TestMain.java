0817854874857d8d799bdd14a0919f57a079c95f
reign2080 <dpffldhkdwp@naver.com>
2017-05-02 15:12:02 +0900

    Update TestMain.java

--- src/com/example/stack/TestMain.java
+++ src/com/example/stack/TestMain.java
@@ -2,6 +2,8 @@ package com.example.stack;
 
 public class TestMain {
 	public static void main(String[] args) {
-		System.out.println("Hello Git!");
-	}
-}
+		System.out.println("하똥");
+	} 
+}                
+ 
+
